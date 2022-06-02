e4cfa51e46a022676bcde73b9964a413e8cc3d0d
Kutay Dönmez <60704340+donmezkutay@users.noreply.github.com>
2022-06-02 18:35:54 +0300

    #First Commit - Upload project codes

---
+++ functions.py
@@ -0,0 +1,155 @@
+import xarray as xr
+import rioxarray
+from datetime import datetime
+from scipy.constants import g
+import geopandas as gpd
+import cartopy
+from shapely.geometry import mapping
+from visjobs.datas import get_ERA5
+import requests
+from bs4 import BeautifulSoup 
+import matplotlib
+import proplot
+import cartopy
+import matplotlib.pyplot as plt
+from visualization_codes import plot_facet_map
+import pandas as pd
+from matplotlib.colors import BoundaryNorm
+import numpy as np
+
+
+def adjust_unit(data, variable):
+    
+    unit_conversion = {
+        'PMSL': 0.01,
+        'RELHUM_2M': 1,
+        'TOT_PREC': 1,
+        'T_2M': 273.15,
+        'U_10M': 3.6,
+        'V_10M': 3.6,
+        'FI': 1/g,
+        'RELHUM': 1,
+        'T': 273.15,
+        'VAR_2T': 273.15,
+        'MSL': 0.01,
+        'VAR_10U': 3.6,
+        'VAR_10V': 3.6,
+        'T': 273.15, 
+        'Z': 1/g,
+        'u10':3.6,
+        'v10':3.6,
+        't2m':273.15,
+        'msl':0.01,
+        'tp':1,
+        'z':1/g,
+        't':273.15
+        
+    }
+    
+    if variable in ['T_2M', 'T', 'VAR_2T', 'T', 't2m', 't']:
+        return data - unit_conversion[variable]
+    
+    return data[variable] * unit_conversion[variable]
+
+def get_average(data, variable, month_start,
+                month_end, *args, **kwargs):
+    
+    data = adjust_unit(data, variable)
+    return data.sel(time = (data['time.month'] >= month_start) &
+                           (data['time.month'] <= month_end),
+                           *args, **kwargs) \
+                           .mean(dim = 'time')
+    
+    
+def get_average_bymonth(data, variable, month_start,
+                        month_end, *args, **kwargs):
+    
+    data = adjust_unit(data, variable)
+    return data.groupby('time.month') \
+               .mean(dim='time') \
+               .sel(month = range(month_start,
+                                  month_end+1),
+                   *args, **kwargs)
+
+def assing_proj_info(data, crs_data,
+                     x_dims, y_dims):
+    
+    # set spatial dimension names
+    data = data.rio.set_spatial_dims(x_dim = x_dims,
+                                     y_dim = y_dims)
+    # write crs info
+    data = data.rio.write_crs(crs_data)
+    
+    return data
+
+def session_accredition(username, password):
+    
+    #start the session
+    session = requests.Session()
+    session.auth = (username, password)
+    
+    return session
+
+def retrieve_era5_data_link(model_level, var_name, year):
+    
+    # server link
+    server_link = fr'https://rda.ucar.edu/thredds/catalog/files/g/ds633.1_nc/e5.moda.an.{model_level}/{year}/catalog.html'
+    
+    # Make a request to the nomads server link
+    page = requests.get(server_link)
+
+    # View the page content
+    soup = BeautifulSoup(page.content)
+
+    # Page content under <a> tag
+    a_tag = soup.find_all('a')
+
+    # get data links
+    data_links = [a.get_text() for a in a_tag if not str(a).find('.nc') == -1]
+    
+    var_link = server_link.replace('catalog.html', '').replace('catalog', 'dodsC') + \
+               [link for link in data_links if var_name in link][0]  
+    
+    return var_link
+
+def concat_season_means(data):
+    
+    seasons = {
+        'spring':slice(3,4,5),
+        'summer':slice(6,7,8),
+        'autumn':slice(9,10,11)
+    }
+
+    seasonal_dt = []
+
+    for season in seasons.keys():
+        seasonal_mean = data.sel(month=seasons[season]).mean(dim='month')
+        seasonal_mean = seasonal_mean.assign_coords(coords={'season':str(season)})
+        seasonal_dt.append(seasonal_mean)
+
+    return xr.concat(seasonal_dt, dim='season')
+
+def clip_to_city(data, shapefile, crs_data, x_dims, y_dims):
+    data= data.rio.set_spatial_dims(x_dim=x_dims, y_dim=y_dims)
+
+    data = data.rio.write_crs(crs_data)
+    
+    clipped = data.rio.clip(shapefile.geometry.apply(mapping), shapefile.crs,
+                            all_touched=True, invert=False)
+    
+    return clipped
+
+def higher_resolution(data, x_dim, y_dim, res):
+    """
+    Interpolates given dataset
+    """
+    
+    # new resolution
+    new_lon = np.linspace(data[x_dim][0], data[x_dim][-1], len(data[x_dim]) * res)
+    new_lat = np.linspace(data[y_dim][0], data[y_dim][-1], len(data[y_dim]) * res)
+    
+    # interpolate
+    new = data.interpolate_na(dim = x_dim).interpolate_na(dim = y_dim)
+    new = new.interp({x_dim:new_lon, y_dim:new_lat})
+    
+    return new
---
+++ visualization_codes.py
@@ -0,0 +1,139 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import salem
+import xarray as xr
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import proplot as plot
+import cartopy
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER , LATITUDE_FORMATTER
+import matplotlib as mpl
+import salem
+import rasterio
+from netCDF4 import Dataset
+import xarray as xr
+import numpy as np
+import cartopy
+import matplotlib.pyplot as plt
+from visjobs.visualize import easy_plot
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER , LATITUDE_FORMATTER
+import matplotlib.colors as mcolors
+from datetime import datetime, timedelta
+import matplotlib.patheffects as PathEffects
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+import cartopy.io.shapereader as shpreader
+import proplot as plot
+import rioxarray
+# gerekli kütüphaneleri yükleyelim
+import xarray as xr
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#from gradient.gradient import Gradient
+import proplot as plot
+import cartopy
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER , LATITUDE_FORMATTER
+import matplotlib as mpl
+import salem
+import rasterio
+from visjobs.visualize import easy_plot
+from matplotlib.colors import BoundaryNorm
+import imageio
+import rioxarray
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import proplot
+import geopandas as gpd
+from matplotlib.colors import BoundaryNorm
+from shapely.geometry import mapping
+
+
+
+def plot_facet_map(data_df, cmap, vmin, vmax, norm, ticks,
+                   crs_data, graphic_no, var_name, model_level,
+                   plevel, method, fig_array):
+    
+    # graphic features
+    cmap = cmap
+    vmin = vmin
+    vmax = vmax
+    norm = norm
+    ticks = ticks
+
+
+    # projection
+    crs_data = crs_data
+
+    # Create Figure -------------------------
+    fig, axs = proplot.subplots(fig_array, 
+                              aspect=10, axwidth=5, proj=crs_data,
+                              hratios=tuple(np.ones(len(fig_array), dtype=int)),
+                              includepanels=True, hspace=-1.40, wspace=0.15)
+
+    for i in range(graphic_no):
+        axs[i].format(lonlim=(18.5, 34.8), latlim=(34, 43),
+                      labels=False, longrid=False, latgrid = False)
+
+
+    # türkiye harici shapeler
+    # Find the China boundary polygon.
+    shpfilename = shpreader.natural_earth(resolution='10m',
+                                                  category='cultural',
+                                                  name='admin_0_countries')
+
+    cts = ['Syria', 'Iraq', 'Iran', 'Azerbaijan', 'Armenia',
+           'Russia', 'Georgia', 'Bulgaria', 'Greece', 'Cyprus',
+           'Northern Cyprus', 'Turkey', 'Albania', 'North Macedonia',
+           'Montenegro', 'Serbia', 'Italy']
+
+
+    for country in shpreader.Reader(shpfilename).records():
+        if country.attributes['ADMIN'] in cts:
+            count_shp = country.geometry
+
+            for i in range(graphic_no):
+                axs[i].add_geometries([count_shp], cartopy.crs.PlateCarree(),
+                                          facecolor='none', edgecolor = 'black',
+                                          linewidth = 0.5, zorder = 2.2,)
+    
+    # graphic
+    for i in range(graphic_no):
+        mesh = axs[i].pcolormesh(data_df['lon'], data_df['lat'],
+                             data_df[i], norm = norm,
+                             cmap = cmap, vmin = vmin, vmax = vmax,
+                             zorder = 2.1)
+
+    # CMAP ----------------------
+
+    cbar = fig.colorbar(mesh, ticks=ticks, loc='b', drawedges = False, shrink=1, space = -0.6, aspect = 50, )
+    cbar.ax.tick_params(labelsize=11, )
+    cbar.set_label(label='{} | Model-ERA5'.format(var_name.upper()), size=16, loc = 'center', y=0.35, weight = 'bold')
+    cbar.outline.set_linewidth(2)
+    cbar.minorticks_off()
+    cbar.ax.get_children()[4].set_color('black')
+    cbar.solids.set_linewidth(1)
+
+
+    # TEXT
+    # Build a rectangle in axes coords
+    left, width = .25, .5
+    bottom, height = .25, .5
+    right = left + width
+    top = bottom + height
+    
+    title_method = data_df.dims[0]
+    for i in range(graphic_no):
+        
+        axs[i].set_title(r'{}: {}'.format(method, data_df[i][title_method].values), fontsize = 12,
+                         loc = 'left', pad = -14, y = 0.01, x=0.020, weight = 'bold',)
+        
+    # savefig    
+    plt.savefig(fr'Pictures/{method}_verif_{var_name}_{model_level}_{plevel}_fig.jpeg',
+                bbox_inches='tight', optimize=False, progressive=True, dpi=300)
